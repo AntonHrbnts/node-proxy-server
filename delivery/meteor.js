fa0c65b738ff7f56f3e9cc830b1a2cb2762eeb0f
Anton Hrabianets <anton.hrabianets@gmail.com>
2024-10-14 15:27:40 +0200

    split to adapter & callback

--- delivery/meteor.js
+++ delivery/meteor.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const meteorsCallback = require('./meteors.logic.js')
+const {response} = require("express");
 const app = express();
 
 app.listen(8000, ()=> {
     console.log("Server started, port: 8000")
 })
 
-app.get('/meteors', (req, res) => meteorsCallback)
+app.get('/meteors', meteorsCallback)
