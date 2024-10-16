35de595d2eeb70d72175b07b8bb52832575ea5a2
Anton Hrabianets <anton.hrabianets@gmail.com>
2024-10-16 14:46:39 +0200

    task-10: meteorsView

--- Delivery/expressInitialization.js
+++ Delivery/expressInitialization.js
@@ -1,10 +1,13 @@
+const config = require("../config/config");
+
 function expressInit() {
+    const config = require("../config/config.js");
     const express = require("express");
     const app = express();
 
     const nunjucks = require('nunjucks')
 
-    nunjucks.configure('templates', {
+    nunjucks.configure(config.template_path, {
         autoescape: true,
         express: app
     })
@@ -12,8 +15,8 @@ function expressInit() {
     app.set('view engine', 'njk')
     app.use(express.json())
 
-    app.listen(8000, () => {
-        console.log("Server started, port: 8000")
+    app.listen(config.port, () => {
+        console.log("Server started, port: " + config.port)
     })
     return app;
 }
---
+++ config/config.js
@@ -0,0 +1,6 @@
+const config = {
+    port: 8000,
+    template_path: 'templates'
+}
+
+module.exports = config
